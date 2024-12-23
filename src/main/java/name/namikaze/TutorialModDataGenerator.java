7f905023f8186dc2a981baf93dd6d58012c6bfa4
NamikazeSatellite <mate.qochaka@gmail.com>
2024-12-23 19:54:05 +0400

    tested git idk

---
+++ src/main/java/name/namikaze/TutorialModDataGenerator.java
@@ -0,0 +1,12 @@
+package name.namikaze;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+// test important messages idk
+public class TutorialModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
